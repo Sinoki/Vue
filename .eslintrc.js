de8df97ce2588f2ada90eae46be40bf08b00b22a
Vanessa Sinoki <75032280+Sinoki@users.noreply.github.com>
2021-04-11 00:26:48 -0300

    Aula 11
    - Criou atoms
    - Buscou pokemons

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'import/prefer-default-export': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
--- src/modules/cards.ts
+++ src/modules/cards.ts
@@ -1,17 +1,35 @@
+import axios from 'axios';
 import { reactive, readonly } from 'vue';
 
 // ------------------Interfaces--------------------------------//
-interface Card {
-    id: number;
-    name: string;
-    price: number;
-    attack: number;
-    defence: number;
-}
 
+interface Sprite {
+  frontDefault: string;
+}
+interface Card {
+  id: number;
+  name: string;
+  sprites: Sprite;
+  abilities?: [];
+  baseExperience?: number;
+  forms?: [];
+  gameIndices?: [];
+  height?: number;
+  heldItems?: [];
+  isDefault?: boolean;
+  locationAreaEncounters?: string;
+  moves?: [];
+  order?: number;
+  pastTypes?: [];
+  species?: Record<string, unknown> ;
+  stats?: [];
+  types?: [];
+  weight?: number;
+}
 interface State {
-    list: Card[];
-    busy: boolean;
+  list: Card[];
+  busy: boolean;
+  nextUrl: string;
 }
 
 // ------------------------------------------------------------//
@@ -20,6 +38,7 @@ interface State {
 const state: State = reactive({
   list: [],
   busy: false,
+  nextUrl: '',
 });
 // -----------------------------------------------------------//
 
@@ -28,7 +47,7 @@ const mutations = {
   setBusy(newValue: boolean) {
     state.busy = newValue;
   },
-  processCard(card:any) {
+  processCard(card: any) {
     // Verificar se existe, buscando o index
     const idx = state.list.findIndex((x) => x.id === card.id);
 
@@ -36,9 +55,9 @@ const mutations = {
     const newCard: Card = {
       id: card.id,
       name: card.name,
-      price: card.price,
-      attack: card.attack,
-      defence: card.defence,
+      sprites: {
+        frontDefault: card.sprites.front_default,
+      },
     };
 
     // Se existir -> edita;
@@ -50,260 +69,43 @@ const mutations = {
       state.list[idx] = newCard;
     }
   },
+
+  setNextUrl(url: string) {
+    state.nextUrl = url;
+  },
 };
 // -------------------------------------------------------//
 
 // ------------------- Actions ------------------------------//
 const actions = {
-  loadCards() {
+  async loadCards() {
     mutations.setBusy(true);
 
-    const mockData = [
-      {
-        id: 1,
-        name: 'Pikachu',
-        price: 10,
-        attack: 5,
-        defence: 10,
-        url: '',
-      },
-      {
-        id: 2,
-        name: 'Bulbasaur',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 3,
-        name: 'Charmander',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 4,
-        name: 'Squirtle',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 5,
-        name: 'Meowth',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 6,
-        name: 'Butterfree',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 7,
-        name: 'Eevee',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 8,
-        name: 'Arbok',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 9,
-        name: 'Mewtwo',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 10,
-        name: 'Magikarp',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 11,
-        name: 'Psyduck',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 12,
-        name: 'Onix',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 13,
-        name: 'Pidgey',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 14,
-        name: 'Primeape',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 15,
-        name: 'Snorlax',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 16,
-        name: 'Scyther',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 17,
-        name: 'Rattata',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 18,
-        name: 'Sandshrew',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 19,
-        name: 'Muk',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 20,
-        name: 'Jigglypuff',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 21,
-        name: 'Zubat',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 22,
-        name: 'Kadabra',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 23,
-        name: 'Venonat',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 24,
-        name: 'Rapidash',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 25,
-        name: 'Hitmonlee',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 26,
-        name: 'Golem',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 27,
-        name: 'Koffing',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 28,
-        name: 'Dodrio',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 29,
-        name: 'Tangela',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 30,
-        name: 'Dragonite',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 31,
-        name: 'Bellsprout',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 32,
-        name: 'Haunter',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-      {
-        id: 33,
-        name: 'Lapras',
-        price: 10,
-        attack: 5,
-        defence: 10,
-      },
-    ];
+    const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=5&offset=0');
+    console.log(res.data);
 
-    console.log('Vamos carregar os cards');
+    mutations.setNextUrl(res.data.next);
 
-    setTimeout(() => {
-      console.log('depois de 2s');
-      mockData.forEach((card) => {
-        console.log(card);
+    actions.loadPokemon(res.data.results);
+  },
+
+  async loadMore() {
+    axios.get(state.nextUrl).then((res) => {
+      console.log(res);
+
+      mutations.setNextUrl(res.data.next);
+      actions.loadPokemon(res.data.results);
+    });
+  },
+
+  async loadPokemon(results: any) {
+    results.forEach((pokemon: any) => {
+      console.log(pokemon);
+      axios.get(pokemon.url).then((p) => {
+        const card = p.data;
         mutations.processCard(card);
       });
-
-      mutations.setBusy(false);
-    }, 2000);
+    });
   },
 };
 // ---------------------------------------------------------//
