12ef10f23e84b9d0d4cef54ff6bc3cd21d09d6da
Vanessa Sinoki <75032280+Sinoki@users.noreply.github.com>
2021-04-14 14:14:51 -0300

    Aula-13
    Carrinho

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,18 +2,24 @@
   <div class="header">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
+
     <span v-if="!isLoggedIn">
       <router-link to="/login">Login</router-link>
     </span>
 
     <span v-else>
-      Bem vindo: <strong>{{ username }}</strong> |
+      Bem vindo: <strong>{{username}}</strong> |
       <span @click="logoutHandler">Logout</span>
     </span>
 
     <div class="header-balance">
-      Balance: <strong>{{ balance }}</strong>
+      Balance: <strong>{{balance}}</strong> |
+
+      <router-link :to="{name: 'Checkout'}">
+      Cart: <strong>{{cartCount}}</strong>
+      </router-link>
     </div>
+
   </div>
 </template>
 
@@ -32,6 +38,7 @@ export default defineComponent({
     const isLoggedIn = computed(() => auth.state.token);
     const username = computed(() => auth.state.username);
     const balance = computed(() => me.state.balance);
+    const cartCount = computed(() => me.state.cart.length);
 
     const logoutHandler = () => {
       auth.actions.logout();
@@ -41,6 +48,7 @@ export default defineComponent({
       isLoggedIn,
       username,
       balance,
+      cartCount,
       logoutHandler,
     };
   },
